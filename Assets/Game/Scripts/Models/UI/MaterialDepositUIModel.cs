246a70c9c56dc5f16fbe06cf36a72e1b2dbb377f
Karel Geyer <karelgeyer@gmail.com>
2024-02-27 18:50:37 +0100

    Adding extra folder to have models more organised, creating new model

--- Assets/Game/Scripts/Models/UI/MaterialDepositUIModel.cs
+++ Assets/Game/Scripts/Models/UI/MaterialDepositUIModel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MaterialDepositUiModel
+public class MaterialDepositUIModel
 {
 	public MaterialType Type { get; set; }
 
---
+++ Assets/Game/Scripts/Models/UI/MaterialStorageUIModel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialStorageUIModel
+{
+	public MaterialType MaterialType;
+
+	public int Value;
+}
