f711ee44c61fb03e5d0ae27ef840e255f3622a80
Karel Geyer <karelgeyer@gmail.com>
2024-02-27 18:46:53 +0100

    removing print

--- Assets/Game/Scripts/App/Characters/NPC.cs
+++ Assets/Game/Scripts/App/Characters/NPC.cs
@@ -64,7 +64,6 @@ public class NPC : MonoBehaviour
 			gameObject.SetActive(true);
 
 		m_agent.destination = position;
-		print(m_agent.destination);
 	}
 
 	public void AssignWorkplace(Building building)
